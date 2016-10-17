4bd5d5ff22d4c3727dcc9c6bdf82b5bb7df256ab
jwells6556 <jwells6556@gmail.com>
2016-10-17 08:56:56 -0400

    jw lab

--- starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -1,12 +1,54 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txt1,txt2,txt3,txt4,txt5,txt6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button button = (Button) findViewById(R.id.submit);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1 = (EditText) findViewById(R.id.edittext1);
+                txt2 = (EditText) findViewById(R.id.edittext2);
+                txt3 = (EditText) findViewById(R.id.edittext3);
+                txt4 = (EditText) findViewById(R.id.edittext4);
+                txt5 = (EditText) findViewById(R.id.edittext5);
+                txt6 = (EditText) findViewById(R.id.edittext6);
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+
+                intent.putExtra("noun1",txt1.getText().toString());
+                intent.putExtra("noun2",txt2.getText().toString());
+                intent.putExtra("adj1",txt3.getText().toString());
+                intent.putExtra("adj2",txt4.getText().toString());
+                intent.putExtra("animal",txt5.getText().toString());
+                intent.putExtra("game",txt6.getText().toString());
+
+                if (!txt1.equals(" ") && !txt2.equals(null) && !txt3.equals(null)
+                        &&!txt4.equals(null) && !txt5.equals(null) && !txt6.equals(null) ) {
+                    startActivity(intent);
+                }
+
+                else
+                    Toast.makeText(getApplicationContext(),"Please fill in all fields!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
@@ -1,5 +1,6 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -14,11 +15,30 @@ import android.widget.Toast;
  */
 public class ResultActivity extends AppCompatActivity {
     private Button mBackButton;
+    TextView result;
+    String [] madlib = new String[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        result = (TextView) findViewById(R.id.result_textview);
+        Intent intent = getIntent();
+
+
+        String noun1= intent.getStringExtra("noun1");
+        String noun2= intent.getStringExtra("noun2");
+        String adj1 = intent.getStringExtra("adj1");
+        String adj2 = intent.getStringExtra("adj2");
+        String animal = intent.getStringExtra("animal");
+        String game = intent.getStringExtra("game");
+
+        result.setText("A good vacation is where you take a trip to " +
+                "some " + adj1 +  " place with your "+ adj2 + " family. Usually you go to some place" +
+                " near a " + noun1 + " or up on a " + noun2 + " A good vacation is one where you " +
+                "can ride " + animal + "and play "+ game);
+
 
         mBackButton = (Button) findViewById(R.id.back_button);
 
